b04d8f78002c99274b521e6c9719f27396cdba0b
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-07-20 23:32:57 +0530

    feat: implement get single task controller

--- Backend/src/controllers/task.controller.js
+++ Backend/src/controllers/task.controller.js
@@ -63,3 +63,38 @@ export const getAllTasksController = async (req, res) => {
 
   }
 };
+
+
+export const getTaskController = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+    const task = await Task.findOne({
+      _id: id,
+      createdBy: req.user.id,
+    });
+
+    if (!task) {
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Task fetched successfully",
+      data: {
+        task,
+      },
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+
+  }
+};
