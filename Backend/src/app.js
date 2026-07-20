8bb0ab29e56b2cf38d83de7195724eeba47bbb34
Sandola Rakesh Sagar <rakeshsagartfi21@gmail.com>
2026-07-21 02:10:13 +0530

    feat: configure CORS for frontend integration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import authRouter from "./routes/auth.routes.js";
 import taskRouter from "./routes/task.routes.js";
@@ -7,6 +8,10 @@ import adminRouter from "./routes/admin.routes.js";
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}))
 
 app.use("/api/v1/auth", authRouter);
 
